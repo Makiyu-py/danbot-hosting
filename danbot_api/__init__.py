9702727691730594f2f4b1e3218c387c06e6357d
Makiyu-py <dankerdanker11@gmail.com>
2021-06-13 19:04:30 +0800

    added version_info on init file

--- danbot_api/__init__.py
+++ danbot_api/__init__.py
@@ -1,6 +1,12 @@
+from collections import namedtuple
+
 from .client import DanBotClient
 from .http import HTTPClient
 from . import helpers
 from .errors import *
 
 __version__ = "1.0"
+
+VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
+
+version_info = VersionInfo(major=1, minor=0, micro=0, releaselevel='final', serial=0)
